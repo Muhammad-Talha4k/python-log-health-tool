574596717fab57893712bf94b49f35c93bf1feaa
Muhammad-Talha4k <muhammadtalhasheikh50@gmail.com>
2026-02-04 14:58:52 +0500

    WIP before merging feature-error-summary

--- src/analyzer.py
+++ src/analyzer.py
@@ -1,2 +1,8 @@
 def count_logs(df):
     return len(df)
+def error_summary(df):
+    return df["level"].value_counts().to_dict()
+
+def group_by_time(df):
+    df["hour"] = df["timestamp"].str[:13]
+    return df.groupby("hour").size()
